5aa7cad42d12b581015e3d2c7da2eb5932436247
Alejandro <alepaubel@alu.edu.gva.es>
2024-02-26 19:48:02 +0100

    soluciono el bug de los peones, falta hacer que el tablero identifique si falta algún rey

---
+++ src/main/java/Comprobaciones.java
@@ -0,0 +1,8 @@
+public class Comprobaciones {
+    public static boolean coordenadaCorrecta(String coordenada){
+        if (coordenada.length()!=2 || (coordenada.charAt(0)<65 || coordenada.charAt(0)>90) || (coordenada.charAt(1)-48>8 || coordenada.charAt(0)-48<1)){
+            return false;
+        }
+        return true;
+    }
+}
